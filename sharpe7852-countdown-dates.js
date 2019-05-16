cc68dda234695e5604466dba048092c9cefe1305
sharpe7852 <sharpe7852@yahoo.com>
2019-05-16 16:09:40 +0430

    Update sharpe7852-countdown-dates.js

--- sharpe7852-countdown-dates.js
+++ sharpe7852-countdown-dates.js
@@ -30,7 +30,7 @@ class CountdownCard extends HTMLElement {
       }
 
       if (age > -1) {
-        var daysLeft = (this.config.show_age ? "age " + age + " in " : "") + daysLeft;
+        var daysLeft = (this.config.show_age ? "" + age + " in " : "") + daysLeft;
       }
       
       line += `
